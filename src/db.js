086e239a20054becb22b7f8fc7052291dd58a3fe
Alejandro <alejandrocruz1987@gmail.com>
2026-01-13 19:19:09 -0300

    backend 2

--- src/db.js
+++ src/db.js
@@ -2,12 +2,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_SSL === 'true'
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 // Función helper para hacer consultas
