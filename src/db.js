d418f508439ee61668bdfe48b296c964f78efac6
Alejandro <alejandrocruz1987@gmail.com>
2026-01-13 19:31:21 -0300

    backend 4

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_PUBLIC_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.DB_SSL === 'true'
     ? { rejectUnauthorized: false }
     : false,
